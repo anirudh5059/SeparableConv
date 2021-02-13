4c3c675b33c24a64bdcd107b4a69723d43409a48
Anirudh <anirudhsingh@iisc.ac.in>
2021-02-13 18:48:07 +0530

    Adding functionality for learning rate scheduling + add option trying out training with various cutoffs of operator norm and logging them to different files

--- proc_nnet.py
+++ proc_nnet.py
@@ -37,12 +37,14 @@ def normalize_google_Conv2D(kern, in_shape):
 
 #Default parameters
 batch_size = 256
-#opt = tf.keras.optimizers.Adam()
-lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-    initial_learning_rate=1e-1,
-    decay_steps=200,
-    decay_rate=5e-4)
-opt = tf.keras.optimizers.SGD(learning_rate = lr_schedule, momentum = 0.9)
+#lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+#    initial_learning_rate=1e-3,
+#    decay_steps=3000,
+#    decay_rate=0.96)
+#lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
+#    boundaries = [3000, 6000, 12000], values = [0.001, 0.0005, 0.0002, 0.00001])
+opt = tf.keras.optimizers.Adam()
+#opt = tf.keras.optimizers.SGD(learning_rate = lr_schedule, momentum = 0.9)
 loss_fn = tf.keras.losses.CategoricalCrossentropy()
   
 #Load the data
@@ -81,66 +83,73 @@ train_dataset =  datagen.flow(x_train, y_train, batch_size=batch_size)
 model = convnet1()
 
 #Training Parameters
-epochs = 40
+epochs = 100 
 linf_norm = False
 l2_norm=False
 k = 10
-google_reg=False
+google_reg=True
 
 
 #Training
-for epoch in range(epochs):
-  print("\nStart of epoch %d" % (epoch,))
+for cutoff in [5,10,20,30]:
+  o_path = "../experiments/ofile"+str(cutoff)+".txt"
+  f = open(o_path, "x")
+  for epoch in range(epochs):
+    print("\nStart of epoch %d" % (epoch,))
 
-# Iterate over the batches of the dataset.
-  for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
+  # Iterate over the batches of the dataset.
+    for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
 
 
-# Open a GradientTape to record the operations run
-# during the forward pass, which enables auto-differentiation.
-    with tf.GradientTape() as tape:
+  # Open a GradientTape to record the operations run
+  # during the forward pass, which enables auto-differentiation.
+      with tf.GradientTape() as tape:
 
-# Run the forward pass of the layer.
-# The operations that the layer applies
-# to its inputs are going to be recorded
-# on the GradientTape.
-      logits = model(x_batch_train, training=True)  # Logits for this minibatch
+  # Run the forward pass of the layer.
+  # The operations that the layer applies
+  # to its inputs are going to be recorded
+  # on the GradientTape.
+        logits = model(x_batch_train, training=True)  # Logits for this minibatch
 
-# Compute the loss value for this minibatch.
-      loss_value = loss_fn(y_batch_train, logits)
+  # Compute the loss value for this minibatch.
+        loss_value = loss_fn(y_batch_train, logits)
 
-# Use the gradient tape to automatically retrieve
-# the gradients of the trainable variables with respect to the loss.
-    grads = tape.gradient(loss_value, model.trainable_weights)
-# Run one step of gradient descent by updating
-# the value of the variables to minimize the loss.
-    opt.apply_gradients(zip(grads, model.trainable_weights))
+  # Use the gradient tape to automatically retrieve
+  # the gradients of the trainable variables with respect to the loss.
+      grads = tape.gradient(loss_value, model.trainable_weights)
+  # Run one step of gradient descent by updating
+  # the value of the variables to minimize the loss.
+      opt.apply_gradients(zip(grads, model.trainable_weights))
 
-#Regularization and logging
-    if(step > len(x_train)/batch_size):
-      break
-    if step % k == 0:
-      for layer in model.layers:
-        if(isinstance(layer, SpaceDepthSepConv2) and (layer.norm_flag == True)):
-          if(linf_norm == True):
-            layer.normalize_linf()
-          if(l2_norm == True):
-            layer.normalize_l2()
-          if(google_reg == True):
-            layer.normalize_google()
-        if(isinstance(layer, tf.keras.layers.Conv2D)):
-          arr = layer.get_weights()
-          in_shape = layer.input_shape[1:3]
-          D, U, V, conv_shape = SVD_Conv_Tensor(arr[0], in_shape)
-          norm = tf.math.reduce_max(D)
-          #print("Unnormalized norm = "+str(norm))
-          if(google_reg == True):
-            n_kern = Clip_OperatorNorm(D, U, V, conv_shape, 0.6*norm)
-            layer.set_weights([n_kern, arr[1]])
-    print("Training loss (for one batch) at step %d: %.4f"% (step, float(loss_value)))
+  #Regularization and logging
+      if(step > len(x_train)/batch_size):
+        break
+      if step % k == 0:
+        for layer in model.layers:
+          if(isinstance(layer, SpaceDepthSepConv2) and (layer.norm_flag == True)):
+            if(linf_norm == True):
+              layer.normalize_linf()
+            if(l2_norm == True):
+              layer.normalize_l2()
+            if(google_reg == True):
+              layer.normalize_google()
+          if(isinstance(layer, tf.keras.layers.Conv2D)):
+            arr = layer.get_weights()
+            in_shape = layer.input_shape[1:3]
+            D, U, V, conv_shape = SVD_Conv_Tensor(arr[0], in_shape)
+            norm = tf.math.reduce_max(D)
+            #print("Unnormalized norm = "+str(norm))
+            if(google_reg == True):
+              n_kern = Clip_OperatorNorm(D, U, V, conv_shape, cutoff)
+              layer.set_weights([n_kern, arr[1]])
+      print("Training loss (for one batch) at step %d: %.4f"% (step, float(loss_value)))
 
-test_loss, test_acc = model.evaluate(x=x_test, y=y_test)
-train_loss, train_accuracy = model.evaluate(x=x_train, y=y_train)
-# Print out the model accuracy 
-print('\nTrain accuracy:', train_accuracy)
-print('\nTest accuracy:', test_acc)
+  test_loss, test_accuracy = model.evaluate(x=x_test, y=y_test)
+  train_loss, train_accuracy = model.evaluate(x=x_train, y=y_train)
+  # Print out the model accuracy 
+  print('\nTrain accuracy:', train_accuracy)
+  print('\nTest accuracy:', test_accuracy)
+  f.write("Test Accuracy: "+str(test_accuracy))
+  f.write("Train Accuracy: "+str(train_accuracy))
+  f.close()
+
